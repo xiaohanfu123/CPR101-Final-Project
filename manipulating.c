616c573e77c8afac99a832e0c403136d98e2dd04
Jarvinia Zhao <jzhao169@myseneca.ca>
2023-11-28 21:02:47 -0500

    Version 2: Finish comments 1.0

--- manipulating.c
+++ manipulating.c
@@ -32,7 +32,7 @@ void manipulating(void) {
     do {
         // Prompt the user for the first string
         printf("Type the 1st string (q - to quit):\n");
-        fgets(string1, BUFFER_SIZE, stdin); // Read the first input string from user
+        fgets(string1, BUFFER_SIZE, stdin); // Read the first input string from user and stores in string1
         // Remove the newline character at the end of the input string
         string1[strlen(string1) - 1] = '\0';
 
@@ -43,7 +43,7 @@ void manipulating(void) {
 
         // Prompt the user for the second string
         printf("Type the 2nd string:\n");
-        fgets(string2, BUFFER_SIZE, stdin); // Read the second input string from user
+        fgets(string2, BUFFER_SIZE, stdin); // Read the second input string from user and stores in string2
         // Remove the newline character at the end of the input string
         string2[strlen(string2) - 1] = '\0'; 
 
@@ -58,31 +58,51 @@ void manipulating(void) {
 
 
 /* Version 2 */
+    //------------- Demo of String Comparing ---------------
+
+    // Display the staring message for user to understand the purpose of this demo
     printf("**** Start of Comparing Strings Demo ****\n");
-    char compare1[BUFFER_SIZE];
-    char compare2[BUFFER_SIZE];
-    int result;
+
+    // Declare 2 Cstring to store the string for comparison
+    char compare1[BUFFER_SIZE]; // first string
+    char compare2[BUFFER_SIZE]; // second string
+    int result; // Store the comparison result
+
+    // Start a loop at least once and will continue until the user enter q to quit
     do {
+        // Prompt the user for the first string
         printf("Type the 1st string to compare (q - to quit):\n");
-        fgets(compare1, BUFFER_SIZE, stdin);
+        fgets(compare1, BUFFER_SIZE, stdin); // Read the first input Cstring from user and stores in compare1
+        // Remove the newline character at the end of the input string
         compare1[strlen(compare1) - 1] = '\0';
+
+        // Check whether the user wants to exit current demo. If user input 'q', then exit the demo
         if (strcmp(compare1, "q") == 0) {
             break;
         }
+        // Prompt the user for the second string
         printf("Type the 2nd string to compare:\n");
-        fgets(compare2, BUFFER_SIZE, stdin);
+        fgets(compare2, BUFFER_SIZE, stdin); // Read the second input Cstring from user and stores in compare2
+        // Remove the newline character at the end of the input string
         compare2[strlen(compare2) - 1] = '\0';
+
+        // Compare compare1 with compare2 and store the result to variable reuslt
         result = strcmp(compare1, compare2);
+
+        // Check the value of reuslt and display corresponding message to user
+        // compare1 < compare2
         if (result < 0) {
             printf("\"%s\" string is less than \"%s\"\n", compare1, compare2);
         }
+        // compare1 = compare2
         else if (result == 0) {
             printf("\"%s\" string is equal to \"%s\"\n", compare1, compare2);
         }
+        // compare1 > compare2 
         else {
             printf("\"%s\" string is greater than \"%s\"\n", compare1, compare2);
         }
-    } while (strcmp(compare1, "q") != 0);
+    } while (strcmp(compare1, "q") != 0); // Continue looping until the user enter 'q' for the compare1
     printf("**** End of Comparing Strings Demo ****\n");
     
 
